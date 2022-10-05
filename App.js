d143d32c5b80e51f5629e451070cd7dc27b1fdf2
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-10-05 13:55:29 -0300

    feat: finish poc

--- App.js
+++ App.js
@@ -1,58 +1,96 @@
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
-import {useState} from "react";
-import base64 from 'react-native-base64';
-import path from 'path'
-import SpeechToTextV1 from 'ibm-watson/speech-to-text/v1';
-import {IamAuthenticator} from "ibm-watson/auth";
-import FileSystem from 'expo-file-system';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { Audio } from "expo-av";
 import axios from "axios";
-import header from "react-native/Libraries/NewAppScreen/components/Header";
 
 export default function App() {
-  const [response, setResponse] = useState('');
+    const [permission, setPermission] = React.useState("");
+    const [recording, setRecording] = React.useState("");
 
-  const transcription = async () => {
-      const formData = new FormData();
-      const audioFile = './records/audio-file.flac'
+    const sendMessage = (recording) => {
+        const formData = new FormData();
+        formData.append("file", {
+            uri: recording.getURI(),
+            name: "test.wav",
+            type: "audio/wav",
+        });
+        axios
+            .post('https://nodered-app-1tdsr-fiap-2021.mybluemix.net/transcription', formData, {
+                responseType: 'text',
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            })
+            .then( async function (response) {
+                console.log(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+                console.error(error.response);
+            });
+    }
 
-      formData.append('msg.payload', {
-          uri: audioFile,
-          type: 'audio/x-flac'
-      })
+    async function startRecording() {
+        try {
+            const permission = await Audio.requestPermissionsAsync();
+            if (permission.status === "granted") {
+                await Audio.setAudioModeAsync({
+                    allowsRecordingIOS: true,
+                    playsInSilentModeIOS: true,
+                });
+                const recording = new Audio.Recording();
+                try {
+                    await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+                    await recording.startAsync();
+                } catch (err) {
+                    console.log(err)
+                }
+                setRecording(recording)
+            } else {
+                setPermission("Please grant permission to app to access microphone");
+            }
+        } catch (err) {
+            console.error("Failed to start recording", err);
+        }
+    }
 
-      axios({
-          method: "post",
-          url: "http://127.0.0.1:1880/transcription",
-          data: formData,
-          headers: { "Content-Type": "multipart/form-data" },
-      })
-          .then(function (response) {
-              //handle success
-              console.log(response);
-          })
-          .catch(function (response) {
-              //handle error
-              console.log(response);
-          });
-  }
+    async function stopRecording() {
+        await recording.stopAndUnloadAsync();
+        let resp = recording
+        setRecording(undefined)
+        sendMessage(resp)
+    }
 
     return (
         <View style={styles.container}>
-          <View style={{flexDirection: 'row', margin: 5}}>
-            <TextInput style={{borderWidth: 3, borderColor: 'lightblue'}} value={response}/>
-          </View>
-          <View style={{flexDirection: 'row', margin: 5}}>
-            <Button title={"Send"} onPress={transcription}/>
-          </View>
+            <Text>{permission}</Text>
+            <Button
+                title={recording ? "Stop Recording" : "Start Recording"}
+                onPress={recording ? stopRecording : startRecording}
+            />
+            <StatusBar style="auto" />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    row: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    fill: {
+        flex: 1,
+        margin: 16,
+    },
+    button: {
+        margin: 16,
+    },
 });
