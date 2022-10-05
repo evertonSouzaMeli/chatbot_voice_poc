aeab1b04d08ee6eebd1e80c3796cdf7e3865c796
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-10-05 13:56:05 -0300

    feat: finish poc

---
+++ records/post.js
@@ -0,0 +1,15 @@
+
+/*axios({
+    method: "post",
+    url: "http://127.0.0.1:1880/transcription",
+    data: formData,
+    headers: { "Content-Type": "multipart/form-data" },
+})
+    .then(function (response) {
+        //handle success
+        console.log(response);
+    })
+    .catch(function (response) {
+        //handle error
+        console.log(response);
+    });*/
